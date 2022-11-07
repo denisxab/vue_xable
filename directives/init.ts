bbba92b638e5862644e5e83ac46f3370ddebcc0b
Your Name <you@example.com>
2022-11-08 00:22:29 +0300

    "Вт 08 ноя 2022 00:22:29" -

--- directives/init.ts
+++ directives/init.ts
@@ -1,3 +1,4 @@
+import vintersection from "./vintersection";
 import vkeydown from "./vkeydown";
 
-export default [vkeydown];
+export default [vkeydown, vintersection];
---
+++ directives/vintersection.ts
@@ -0,0 +1,63 @@
+/*
+Обработка события достижения - достижение указного элемента.
+
+Это можно использовать для бесконечной ленты постов, 
+для динамической подгруздки постов при достижения конца спика.
+
+
+:::Пример использования:::
+<template>
+    <div>
+        <transition-group name="u-list">
+            <div v-for="p in files">
+                {{ p }}
+            </div>
+        </transition-group>
+        <div v-intersection="ОбработкаСобытия" class="observer">.</div>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    methods: {
+        ОбработкаСобытия() {
+            
+        },
+    },
+};
+</script>
+:::::::::::::::::::::::::::::
+
+*/
+
+interface Tbinding {
+	value: any;
+}
+
+export default {
+	name: "intersection",
+	mounted(elm: HTMLElement, binding: Tbinding) {
+		/*
+        Добавить обработку события - Долистать до указного элемента 
+        */
+		const callback = (entries: any, observer: any) => {
+			if (entries[0].isIntersecting) {
+				// Момент пересечения
+				console.log("Пересекли observer");
+				// Вызываем событие достижение конца, для  компанента
+				binding.value();
+			} else {
+				// Момент выхода из пересечения
+				console.log("Не Пересекли observer");
+			}
+		};
+		const observer = new IntersectionObserver(
+			// Это функция вызовется при пересечение объекта
+			callback,
+			{
+				// При каком проценте видимости должен срабатывать `callback`
+				threshold: 1.0,
+			}
+		);
+		observer.observe(elm);
+	},
+};
